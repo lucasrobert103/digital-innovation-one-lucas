9dd6f5d6b0f6fcd7c6521601a5369eb0b4f25347
lucas robert dos santos bueno <69163019+lucasrobert103@users.noreply.github.com>
2021-03-23 19:02:05 -0300

    Add files via upload

---
+++ DesafiosJava/Numeros_Impares.java
@@ -0,0 +1,17 @@
+package DesafiosJava; // esse código você deve retirar para rodar na plataforma Dio
+
+import java.util.Scanner;
+
+// troque essa classe para public class para rodar na plataforma Dio
+class MinhaTerceiraClasse {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num = entrada.nextInt();
+        for (int i = 0; i <= num; i++ ) {
+          if (i % 2 == 1) //complete seu codigo aqui
+            System.out.println(i);
+      
+    }
+  }
+}
